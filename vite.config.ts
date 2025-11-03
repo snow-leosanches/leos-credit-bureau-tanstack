3d67cf964d763c114d47e3887388e427ec15ee65
Leo Sanches <leonel.sanches@snowplowanalytics.com>
2025-11-03 11:09:24 -0800

    Configurations for Vercel.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite';
 
 const config = defineConfig({
   ssr: {
@@ -16,6 +17,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    nitro(),
   ],
 })
 
